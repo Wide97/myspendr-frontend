b215f36643aabf4815d6817f9cf4aeda56d80f55
Wide97 <mwidesott@gmail.com>
2025-05-25 16:09:28 +0200

    fix2

--- src/components/TelegramQR.jsx
+++ src/components/TelegramQR.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import QRCode from "qrcode.react"; 
+import { QRCodeCanvas } from "qrcode.react";
 import "./TelegramQR.scss";
 
 const TelegramQR = ({ telegramToken }) => {
-  const botUsername = "myspendrbot"; 
-  const link = `https://t.me/${botUsername}?start=${telegramToken}`;
+    const botUsername = "myspendrbot";
+    const link = `https://t.me/${botUsername}?start=${telegramToken}`;
 
-  return (
-    <div className="telegram-qr-container">
-      <h2>📲 Collega Telegram</h2>
-      <p>Scansiona il codice o clicca per aprire direttamente il bot:</p>
+    return (
+        <div className="telegram-qr-container">
+            <h2>📲 Collega Telegram</h2>
+            <p>Scansiona il codice o clicca per aprire direttamente il bot:</p>
 
-      <a href={link} target="_blank" rel="noreferrer">
-        <QRCode value={link} size={200} bgColor="transparent" fgColor="#c1ff00" />
-        <p className="telegram-link">{link}</p>
-      </a>
-    </div>
-  );
+            <a href={link} target="_blank" rel="noreferrer">
+                <QRCodeCanvas value={link} size={200} bgColor="transparent" fgColor="#c1ff00" />
+                <p className="telegram-link">{link}</p>
+            </a>
+        </div>
+    );
 };
 
 export default TelegramQR;
